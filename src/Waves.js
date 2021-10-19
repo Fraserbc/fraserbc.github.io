73facda6ad05f7de0e2b8f9116f4bc61d569a8f9
Fraserbc <fraserbcprice@gmail.com>
2021-10-19 23:36:26 +0100

    Disable wave background on low-end hardware

--- src/Waves.js
+++ src/Waves.js
@@ -1,28 +1,38 @@
 import React from 'react';
 import WAVES from 'vanta/dist/vanta.waves.min';
 import * as THREE from 'three';
+import mapboxgl from '@mapbox/mapbox-gl-supported';
 
 class Waves extends React.Component {
     constructor() {
         super();
         this.vantaRef = React.createRef();
+
+        // Test for WebGL Hardware Mode support
+        this.webglHWMode = mapboxgl.supported({failIfMajorPerformanceCaveat: true});
     }
     componentDidMount() {
-        this.vantaEffect = WAVES({
-            el: this.vantaRef.current,
-            THREE: THREE,
-            mouseControls: false,
-            touchControls: false,
-            gyroControls: false,
-        });
+        if(this.webglHWMode) {
+            this.vantaEffect = WAVES({
+                el: this.vantaRef.current,
+                THREE: THREE,
+                mouseControls: false,
+                touchControls: false,
+                gyroControls: false,
+            });
+        }
     }
     componentWillUnmount() {
-        if (this.vantaEffect) this.vantaEffect.destroy();
+        if(this.webglHWMode && this.vantaEffect) {
+            this.vantaEffect.destroy();
+        }
     }
     render() {
-        return (
-            <div id="waves-el" ref={this.vantaRef}></div>
-        );
+        if(this.webglHWMode) {
+            return (
+                <div id="waves-el" ref={this.vantaRef}></div>
+            );
+        }
     }
 }
 
