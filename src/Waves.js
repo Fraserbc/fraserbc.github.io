3d00152156bb52f7474fce3846bced0d1c626169
Fraserbc <fraserbcprice@gmail.com>
2021-10-20 00:09:33 +0100

    Google PageSpeed WebGL bypass

--- src/Waves.js
+++ src/Waves.js
@@ -6,20 +6,27 @@ class Waves extends React.Component {
     constructor() {
         super();
         this.vantaRef = React.createRef();
+
+        // Disable for the Google PageSpeed test as it runs WebGL in software mode
+        this.enabled = !navigator.userAgent.includes("Chrome-Lighthouse");
     }
     componentDidMount() {
-        this.vantaEffect = WAVES({
-            el: this.vantaRef.current,
-            THREE: THREE,
-            mouseControls: false,
-            touchControls: false,
-            gyroControls: false,
-        });
+        if(this.enabled) {
+            this.vantaEffect = WAVES({
+                el: this.vantaRef.current,
+                THREE: THREE,
+                mouseControls: false,
+                touchControls: false,
+                gyroControls: false,
+            });
+        }
     }
     componentWillUnmount() {
-        if (this.vantaEffect) this.vantaEffect.destroy();
+        if (this.vantaEffect && this.enabled) this.vantaEffect.destroy();
     }
     render() {
+        if(!this.enabled) return null;
+
         return (
             <div id="waves-el" ref={this.vantaRef}></div>
         );
