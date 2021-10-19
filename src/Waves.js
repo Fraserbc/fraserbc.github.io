9406e0e7c4d0cda1ef7333da905db8874f5a3f29
Fraserbc <fraserbcprice@gmail.com>
2021-10-20 00:03:02 +0100

    Removing WebGL hardware testing

--- src/Waves.js
+++ src/Waves.js
@@ -1,40 +1,28 @@
 import React from 'react';
 import WAVES from 'vanta/dist/vanta.waves.min';
 import * as THREE from 'three';
-import mapboxgl from '@mapbox/mapbox-gl-supported';
 
 class Waves extends React.Component {
     constructor() {
         super();
         this.vantaRef = React.createRef();
-
-        // Test for WebGL Hardware Mode support
-        this.webglHWMode = mapboxgl.supported({failIfMajorPerformanceCaveat: true});
     }
     componentDidMount() {
-        if(this.webglHWMode) {
-            this.vantaEffect = WAVES({
-                el: this.vantaRef.current,
-                THREE: THREE,
-                mouseControls: false,
-                touchControls: false,
-                gyroControls: false,
-            });
-        }
+        this.vantaEffect = WAVES({
+            el: this.vantaRef.current,
+            THREE: THREE,
+            mouseControls: false,
+            touchControls: false,
+            gyroControls: false,
+        });
     }
     componentWillUnmount() {
-        if(this.webglHWMode && this.vantaEffect) {
-            this.vantaEffect.destroy();
-        }
+        if (this.vantaEffect) this.vantaEffect.destroy();
     }
     render() {
-        if(this.webglHWMode) {
-            return (
-                <div id="waves-el" ref={this.vantaRef}></div>
-            );
-        } else {
-            return null;
-        }
+        return (
+            <div id="waves-el" ref={this.vantaRef}></div>
+        );
     }
 }
 
